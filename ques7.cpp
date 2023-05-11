7ab7b1b8eb9739d7ac94a2ae042d849d1f9b0734
himanshu09riv <132256183+himanshu09riv@users.noreply.github.com>
2023-05-11 18:56:56 +0700

    Create ques7.cpp

---
+++ ques7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+/*swapping of two numbers using third variable
+*/
+{int a,b,t;
+cout<<"enter two numbers ";
+cin>>a>>b;
+t=a;
+a=b;
+b=t;
+cout<<"after swapping then are"<<a<<"and"<<b<<endl;
+
+    return 0;
+}
