32081594612d53b968cc48b413689d527195e8d4
himanshu09riv <132256183+himanshu09riv@users.noreply.github.com>
2023-05-11 18:55:51 +0700

    Create ques6.cpp

---
+++ ques6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+/*checking two numbers whether they are equal or not
+
+
+*/
+int main()
+{int a,b;
+cout<<"enter two numbers to check";
+cin>>a>>b;
+if(a==b)
+cout<<"a and b are equal";
+else
+cout<<" a and b are not equal ";
+    return 0;
+}
