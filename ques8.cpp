0428554f1a20b1488f9a3b272b387dc767dc0c8d
himanshu09riv <132256183+himanshu09riv@users.noreply.github.com>
2023-05-11 18:57:53 +0700

    Create ques8.cpp

---
+++ ques8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+    /*
+    enter temprature in centigrade convert it into farenheit
+    */
+{float F,C;
+cout<<"enter temperature in centigrade";
+cin>>C;
+F=C*9/5+32;
+cout<<"temperature in farenheit"<<F;
+    return 0;
+}
